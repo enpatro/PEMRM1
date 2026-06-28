7877dbe24437f90b77ad990a27577189c95e7768
EPatro <71399081+enpatro@users.noreply.github.com>
2026-06-28 22:59:34 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,15 @@
 // Replace below values from Firebase Project Settings > Web app config
-export const firebaseConfig = {
-  apiKey: "PASTE_API_KEY",
-  authDomain: "PASTE_PROJECT_ID.firebaseapp.com",
-  projectId: "PASTE_PROJECT_ID",
-  storageBucket: "PASTE_PROJECT_ID.appspot.com",
-  messagingSenderId: "PASTE_SENDER_ID",
-  appId: "PASTE_APP_ID"
+//export const firebaseConfig = {
+   const firebaseConfig = {
+apiKey: "AIzaSyC5ryhOJtqVIiWEYLzLB2gmzJZ-Quk5Ars",
+authDomain: "pemrm1-b5ed6.firebaseapp.com",
+projectId: "pemrm1-b5ed6",
+storageBucket: "pemrm1-b5ed6.firebasestorage.app",
+messagingSenderId: "54528107149",
+appId: "1:54528107149:web:64bd1e3e2b71d89f9138c4"
+
+
+
+
+  
 };
